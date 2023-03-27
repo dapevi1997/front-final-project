09e778106e4119d81ffeee57f7280a74f6c80780
dapevi1997 <dapevi97@gmail.com>
2023-03-27 16:19:40 -0500

    Services auth and roles added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import firebase from 'firebase/compat/app'
+import { AuthService } from './services/auth.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'front-final-project';
+
+  constructor(){}
+
+  ngOnInit(): void {
+    firebase.initializeApp({
+      apiKey: "AIzaSyChv3pr3yYyHrITBAfNQrB_46SzPQy6m2k",
+      authDomain: "final-project-e55b6.firebaseapp.com"
+ 
+    });
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import firebase from 'firebase/compat/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  token!: string;
 
-  constructor() { }
+  constructor(private router$: Router) { }
+
+  async login(email: string, password: string) {
+    await firebase.auth().signInWithEmailAndPassword(email, password).then(
+
+      response => {
+        firebase.auth().currentUser?.getIdToken().then(
+          token => {
+            this.token = token;
+            localStorage.setItem("token", this.token)
+            //this.cookie$.set("token", this.token);
+
+            this.router$.navigate(['/']);
+          }
+        );
+      }
+
+    );
+
+  }
+
+  getIdToken() {
+   
+    return localStorage.getItem("token");
+  }
+
+  logout() {
+    firebase.auth().signOut().then(() => {
+      this.token = "";
+      localStorage.setItem("token", this.token);
+      localStorage.setItem("role", "");
+
+      window.location.reload();
+    });
+  }
+
+
 }
