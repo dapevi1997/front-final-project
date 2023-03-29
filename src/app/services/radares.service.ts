216fbe3da5dd3242218eb66be5bc76a357d09e0c
dsbpajedrez <96492716+dsbpajedrez@users.noreply.github.com>
2023-03-29 16:33:39 -0500

    new: Servicio listar radares

---
+++ src/app/services/radares.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RadarI } from '../interfaces/radar.interface';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RadarService {
+
+
+  private url: string = 'http://localhost:8080/api/';
+
+
+  constructor(private http: HttpClient) {}
+  getRadares() : Observable<RadarI[]> {
+    let direction = this.url + 'listarRadares';
+    return this.http.get<RadarI[]>(direction);
+  }
+
+
+
+}
