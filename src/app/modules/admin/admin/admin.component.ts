f9249fbd1a6961a9919942bdf1a379006feb80f6
dapevi1997 <dapevi97@gmail.com>
2023-03-30 00:12:17 -0500

    List users implemented

--- src/app/modules/admin/admin/admin.component.ts
+++ src/app/modules/admin/admin/admin.component.ts
@@ -1,23 +1,10 @@
 import { Component } from '@angular/core';
-// import { ToastrModule, ToastrService } from 'ngx-toastr';
-import { MessageService } from 'primeng/api';
-import { NgbModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css'],
-  providers:[MessageService]
+  styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
-  constructor(
-    private modalService: NgbModal,
-    private messageService: MessageService,
-    // private toastr: ToastrService
-  ){
-
-  }
-  openVerticallyCentered(content: any) {
-    this.modalService.open(content, { centered: true });
-  }
 
 }
