cbcb158f9b86e24fc41d5eeec62460e6daa89d42
JULIAN MAZO <82735837+BERTOTEM@users.noreply.github.com>
2023-04-01 17:26:26 -0500

    punto de control

--- src/app/modules/operations/radar-leagues/radar-leagues.component.ts
+++ src/app/modules/operations/radar-leagues/radar-leagues.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LigaI } from 'src/app/interfaces/liga.interface';
+import { LigaService } from 'src/app/services/liga.service';
 
 @Component({
   selector: 'app-radar-leagues',
   templateUrl: './radar-leagues.component.html',
   styleUrls: ['./radar-leagues.component.css']
 })
-export class RadarLeaguesComponent {
+export class RadarLeaguesComponent implements OnInit {
+
+  ligas: LigaI[] | any;
+  ligas2: LigaI[] | any;
+  id!:string;
+
+  constructor(
+    private ligaSvr : LigaService
+  ){}
+
+  ngOnInit(): void {
+    this.traerLigas();
+  }
+
+  traerLigas(): void {
+    this.ligaSvr.traerTodo().subscribe((data) => {
+      this.ligas = data;
+      this.ligas2=data?.aprendices?.find(nombre => console.log(nombre)
+      )
+    });
+  }
 
 }
